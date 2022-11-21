fd59238e1a57b2d7d0381cd5656795050c1b8179
Robertosnap <robertosnap@pm.me>
2022-11-21 15:17:32 +0100

    Added admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -3,24 +3,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 export default function IndexPage() {
-	// const utils = trpc.useContext();
-	// const {data, error, isLoading} = trpc.user.list.useQuery(
-	//   {
-	//     limit: 5,
-	//   },
-	//   {
-	//     getPreviousPageParam(lastPage) {
-	//       return lastPage.nextCursor;
-	//     },
-	//   },
-	// );
-
-	// const addUser = trpc.user.add.useMutation({
-	//   async onSuccess() {
-	//     // refetches posts after a post is added
-	//     await utils.user.list.invalidate();
-	//   },
-	// });
 	const { data: session } = useSession();
 
 	return <Container>Hello</Container>;
